b584c6f35763d3444dd09b09adf6984a6ae6f274
teguhwin8 <43848500+teguhwin8@users.noreply.github.com>
2020-10-28 12:27:44 +0700

    TodoList

--- src/App.js
+++ src/App.js
@@ -1,81 +1,117 @@
 import React, { Component } from "react"
 import "./App.css"
+import List from "./List"
 
 // Timer Component
-class Timer extends Component {
+// class Timer extends Component {
+// 	constructor(props) {
+// 		super(props)
+// 		this.state = {
+// 			time: props.start,
+// 		}
+// 	}
+
+// 	componentDidMount() {
+// 		this.addInterval = setInterval(() => this.increase(), 1000)
+// 	}
+
+// 	componentWillUnmount() {
+// 		clearInterval(this.addInterval)
+// 	}
+
+// 	increase() {
+// 		this.setState((state, props) => ({
+// 			time: parseInt(state.time) + 1,
+// 		}))
+// 	}
+
+// 	render() {
+// 		return (
+// 			<div>
+// 				<h1>{this.state.time} Detik</h1>
+// 			</div>
+// 		)
+// 	}
+// }
+
+// Handle Event with Function (Clicker)
+// function Clicker() {
+// 	function handleClick(e) {
+// 		alert("Halo saya berhasil klik")
+// 		e.preventDefault()
+// 	}
+
+// 	return <button onClick={handleClick}>Klik Saya</button>
+// }
+
+// Handle Event with Class (Toggle)
+// class Toggle extends Component {
+// 	constructor(props) {
+// 		super(props)
+// 		this.state = {
+// 			toggleStatus: false,
+// 		}
+
+// 		this.handleClick = this.handleClick.bind(this)
+// 	}
+
+// 	handleClick() {
+// 		this.setState((state) => ({
+// 			toggleStatus: !state.toggleStatus,
+// 		}))
+// 	}
+
+// 	render() {
+// 		return (
+// 			<button onClick={this.handleClick}>
+// 				{this.state.toggleStatus ? "Hidup" : "Mati"}
+// 			</button>
+// 		)
+// 	}
+// }
+
+export class App extends Component {
 	constructor(props) {
 		super(props)
 		this.state = {
-			time: props.start,
+			todoItem: "",
+			items: [],
 		}
 	}
 
-	componentDidMount() {
-		this.addInterval = setInterval(() => this.increase(), 1000)
+	handleChange = (event) => {
+		this.setState({
+			todoItem: event.target.value,
+		})
 	}
 
-	componentWillUnmount() {
-		clearInterval(this.addInterval)
-	}
-
-	increase() {
-		this.setState((state, props) => ({
-			time: parseInt(state.time) + 1,
-		}))
+	handleSubmit = (event) => {
+		event.preventDefault()
+		if (this.state.todoItem !== "") {
+			this.setState({
+				todoItem: "",
+				items: [...this.state.items, this.state.todoItem],
+			})
+		}
 	}
 
 	render() {
 		return (
 			<div>
-				<h1>{this.state.time} Detik</h1>
-			</div>
-		)
-	}
-}
-
-// Handle Event with Function (Clicker)
-function Clicker() {
-	function handleClick(e) {
-		alert("Halo saya berhasil klik")
-		e.preventDefault()
-	}
-
-	return <button onClick={handleClick}>Klik Saya</button>
-}
-
-// Handle Event with Class (Toggle)
-class Toggle extends Component {
-	constructor(props) {
-		super(props)
-		this.state = {
-			toggleStatus: false,
-		}
-
-		this.handleClick = this.handleClick.bind(this)
-	}
-
-	handleClick() {
-		this.setState((state) => ({
-			toggleStatus: !state.toggleStatus,
-		}))
-	}
-
-	render() {
-		return (
-			<button onClick={this.handleClick}>
-				{this.state.toggleStatus ? "Hidup" : "Mati"}
-			</button>
-		)
-	}
-}
-
-export class App extends Component {
-	render() {
-		return (
-			<div className="App">
 				{/* <Timer start="0" /> */}
 				{/* <Clicker /> */}
-				<Toggle />
+				{/* <Toggle /> */}
+
+				<form onSubmit={this.handleSubmit}>
+					<input
+						type="text"
+						onChange={this.handleChange}
+						value={this.state.todoItem}
+					/>
+					<button type="submit">Add</button>
+				</form>
+
+				<List items={this.state.items} />
 			</div>
 		)
 	}
