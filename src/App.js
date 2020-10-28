5f3fbd18251f28d619ed4e048e7e63fa586d93ee
teguhwin8 <43848500+teguhwin8@users.noreply.github.com>
2020-10-28 11:50:06 +0700

    handle event

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react"
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Timer Component
+class Timer extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			time: props.start,
+		}
+	}
+
+	componentDidMount() {
+		this.addInterval = setInterval(() => this.increase(), 1000)
+	}
+
+	componentWillUnmount() {
+		clearInterval(this.addInterval)
+	}
+
+	increase() {
+		this.setState((state, props) => ({
+			time: parseInt(state.time) + 1,
+		}))
+	}
+
+	render() {
+		return (
+			<div>
+				<h1>{this.state.time} Detik</h1>
+			</div>
+		)
+	}
 }
 
-export default App;
+// Handle Event with Function (Clicker)
+function Clicker() {
+	function handleClick(e) {
+		alert("Halo saya berhasil klik")
+		e.preventDefault()
+	}
+
+	return <button onClick={handleClick}>Klik Saya</button>
+}
+
+// Handle Event with Class (Toggle)
+class Toggle extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			toggleStatus: false,
+		}
+
+		this.handleClick = this.handleClick.bind(this)
+	}
+
+	handleClick() {
+		this.setState((state) => ({
+			toggleStatus: !state.toggleStatus,
+		}))
+	}
+
+	render() {
+		return (
+			<button onClick={this.handleClick}>
+				{this.state.toggleStatus ? "Hidup" : "Mati"}
+			</button>
+		)
+	}
+}
+
+export class App extends Component {
+	render() {
+		return (
+			<div className="App">
+				{/* <Timer start="0" /> */}
+				{/* <Clicker /> */}
+				<Toggle />
+			</div>
+		)
+	}
+}
+
+export default App
